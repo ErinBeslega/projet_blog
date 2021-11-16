a0d6c0c8eddb781039249625aa33e3037179aef8
Erenax <miannayneo@gmail.com>
2021-11-17 00:18:37 +0100

    test 1.4
    le style avant tout

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_login.css">
     <title>Page de connexion</title>
 </head>
 
@@ -13,9 +14,11 @@
     <?php
         require "connexion.php";
     ?>
+    
     <form method="POST" action="traite_login.php">
+    <h1>Connectez-vous</h1>
         <label for="login">
-            Votre login : <input type="text" name="login" placeholder="login" required="required">
+            <input type="text" name="login" placeholder="Login" required="required">
         </label>
 
         <?php   // isset = is set ?   
@@ -27,7 +30,7 @@
         ?>
         <br>
         <label for="mdp">
-            Votre mot de passe : <input type="password" name="password" placeholder="mot de passe" required="required">
+            <input type="password" name="password" placeholder="Mot de passe" required="required">
         </label>
 
         <?php
@@ -37,7 +40,10 @@
             }
         ?>
         <br>
-        <input type="submit" value="Connexion">
+        <div class="disp">
+            <input class="connect"type="submit" value="Connexion">
+        </div>
+        
     </form>
 
 
