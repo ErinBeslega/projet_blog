afe68b088b9e608b3c294f4244d13fc10fa1b10d
ErinBeselga <erinbeselga@gmail.com>
2021-11-22 11:16:14 +0100

    bdd 2

--- projet_blog.sql
+++ projet_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 16 nov. 2021 à 16:47
+-- Généré le : lun. 22 nov. 2021 à 11:14
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.2
 
@@ -33,6 +33,17 @@ CREATE TABLE `billets` (
   `contenu_b` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `billets`
+--
+
+INSERT INTO `billets` (`id_billet`, `date_b`, `contenu_b`) VALUES
+(1, '2021-11-22', 'Article 1 \r\nTitre \r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n'),
+(2, '2021-12-22', 'Article 2\r\nTitre \r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n'),
+(3, '2022-01-22', 'Article 3\r\nTitre \r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n'),
+(4, '2022-02-22', 'Article 5\r\nTitre \r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n'),
+(5, '2022-02-22', 'Article 5\r\nTitre \r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\r\n');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +58,16 @@ CREATE TABLE `commentaires` (
   `ex_utilisateurs` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id_commentaire`, `date_c`, `contenu_c`, `ex_billets`, `ex_utilisateurs`) VALUES
+(1, '2021-11-23', 'L\'article est top !', 1, 1),
+(2, '2021-11-24', 'Vraiment nul', 1, 2),
+(3, '2021-12-25', 'Un super article pour Noel ! ', 2, 3),
+(4, '2022-01-28', 'J\'adore cet article ! ', 3, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +79,15 @@ CREATE TABLE `utilisateurs` (
   `login` varchar(255) CHARACTER SET utf8 NOT NULL,
   `mot_de_passe` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_user`, `login`, `mot_de_passe`) VALUES
+(1, 'test', '12'),
+(2, 'erin', '123'),
+(3, 'neo', '123');
 
 --
 -- Index pour les tables déchargées
@@ -89,19 +119,19 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `billets`
 --
 ALTER TABLE `billets`
-  MODIFY `id_billet` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_billet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
