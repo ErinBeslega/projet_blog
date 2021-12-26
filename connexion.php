2fb03ac35a54dacd099efaa771310a0ddd9e1014
ErinBeselga <erinbeselga@gmail.com>
2021-12-26 01:02:18 +0100

    test final

--- connexion.php
+++ connexion.php
@@ -1,3 +1,4 @@
 <?php
+// $db = new PDO('mysql:host=localhost;dbname=erin.beselga-proenca_db;port=3306;charset=utf8', 'root','');
 $db = new PDO('mysql:host=localhost;dbname=projet_blog;port=3306;charset=utf8', 'root','');
 ?>
